9472c8325245ef3d703e50f4957b69a63c30f945
Darren Fogleson <36738922+DarrenFogleson@users.noreply.github.com>
2018-03-29 11:06:21 -0400

    dog.py update

--- dog.py
+++ dog.py
@@ -2,22 +2,47 @@ class Dog:
     """ This is the beginning of a class for the domestic dog """
     
     def __init__(self, name):
+        """ another comment """
         self.name = name
 
     def add_weight(self, weight):
         self.weight = weight
 
+    
+
 
 c = Dog('Rag')
-# c.name = "Fluffy"
+c.add_weight(15)
+c.breed = ('Doge')
+c.color = ('Beige with black underside')
+c.age = ('2.5 years')
 
 x = Dog('Gingo')
-# x.name = "Spike"
-
 x.add_weight(42)
-# x.add_wieght(12)
-
+x.breed = ('Akita')
+x.color = ('White with gray stripe')
+x.age = ('5 years')
+#Dog One
+print("Name:")
 print(c.name)
-
+print("Weight:")
+print(c.weight)
+print("Breed:")
+print(c.breed)
+print("Color:")
+print(c.color)
+print("Age:")
+print(c.age)
+#Dog Two      
+print("Name:")
 print(x.name)
+print("Weight:")
 print(x.weight)
+print("Breed:")
+print(x.breed)
+print("Color:")
+print(x.color)
+print("Age:")
+print(x.age)
+
+
